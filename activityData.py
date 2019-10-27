fa300812144dc5c4e880acaa6af0a2ea47be4afd
xdrond <xdrond@gmail.com>
2019-10-27 21:28:30 +0700

    Initial commit

---
+++ activityData.py
@@ -0,0 +1,77 @@
+import sqlite3
+import datetime
+import csv
+
+""" Установка промежутка,  в который собирается статистика """
+# - 7 hours
+# Указание отметок
+timeStart = datetime.datetime(2019, 10, 21, 12, 40)
+timeEnd = datetime.datetime(2019, 10, 21, 16, 30)
+
+# Конвертирование в timestamp
+tsStart = int(datetime.datetime.timestamp(timeStart))
+tsEnd = int(datetime.datetime.timestamp(timeEnd))
+print(tsStart)
+print(tsEnd)
+
+""" Запрос списка сердцебиения и подключение к БД """
+conn = sqlite3.connect('data/db21_10_2019_1500_1558.sqlite')
+cursor = conn.cursor()
+
+cursor.execute("SELECT * FROM heartrate WHERE time BETWEEN ? AND ?", (tsStart + 1, tsEnd))
+hrOut = cursor.fetchall()
+
+""" Запрос списка шагов и закрытие соединения с БД"""
+cursor.execute("SELECT steps, time FROM steps WHERE time BETWEEN ? AND ?", (tsStart, tsEnd))
+stepsOut = cursor.fetchall()
+conn.close()
+
+# Debug
+# print(hrOut)
+# print(hrOut.__len__())
+# print(type(hrOut))
+# print(stepsOut)
+# print(stepsOut.__len__())
+# print(type(stepsOut))
+
+""" Перенос данных из списка в массив, отбросив пустые ячейки """
+"""
+hrOut[i][1] - проход по пульсу
+hrOut[i][0] - проход по ts
+
+stepsOut[i][0] - проход по шагам
+stepsOut[i][1] - проход по ts
+"""
+hrList = [["Date", "Heartrate"]]
+hrTemp = []
+for i in range(len(hrOut)):
+    hrTemp.append(str(datetime.datetime.fromtimestamp(hrOut[i][0])))
+    hrTemp.append(hrOut[i][1])
+    hrList.append(hrTemp[:])
+    hrTemp.clear()
+
+stepsList = [["Date", "Steps"]]
+stepsTemp = []
+for i in range(len(stepsOut)):
+    stepsTemp.append(str(datetime.datetime.fromtimestamp(stepsOut[i][1])))
+    stepsTemp.append(stepsOut[i][0])
+    stepsList.append(stepsTemp[:])
+    stepsTemp.clear()
+
+# Debug
+# print(hrList)
+# print(len(hrList))
+# print(stepsList)
+# print(len(stepsList))
+
+myFile = open('hrdata.csv', 'w')
+with myFile:
+    writer = csv.writer(myFile)
+    writer.writerows(hrList)
+
+myFile = open('stepsdata.csv', 'w')
+with myFile:
+    writer = csv.writer(myFile)
+    writer.writerows(stepsList)
+
+print("Writing complete")
---
+++ plotlytesting.py
@@ -0,0 +1,35 @@
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
+import pandas as pd
+
+hrDf = pd.read_csv("hrdata.csv")
+
+stepsDf = pd.read_csv("stepsdata.csv")
+
+
+fig = make_subplots(specs=[[{"secondary_y": True}]])
+fig.add_trace(go.Scatter(
+    x=hrDf.Date,
+    y=hrDf['Heartrate'],
+    name="Heart rate",
+    line_color='deepskyblue',
+    opacity=0.8), secondary_y=True, )
+
+fig.add_trace(go.Scatter(
+    x=stepsDf.Date,
+    y=stepsDf['Steps'],
+    name="Steps",
+    line_color='dimgray',
+    opacity=0.8), secondary_y=False, )
+
+# Set x-axis title
+fig.update_xaxes(title_text="<b>Timeline</b>")
+
+# Set y-axes titles
+fig.update_yaxes(title_text="<b>Heart rate</b>", secondary_y=False)
+fig.update_yaxes(title_text="<b>Steps</b>", secondary_y=True)
+
+fig.update_layout(title_text='Heart rate with steps.',
+                  xaxis_rangeslider_visible=True)
+
+fig.show()
