2cdd9353fc168ff3bfd845af1b9ea2f07464f3d3
xdrond <xdrond@gmail.com>
2019-11-14 02:54:14 +0700

    done

--- activityData.py
+++ activityData.py
@@ -2,83 +2,78 @@ import csv
 import datetime
 import sqlite3
 
-""" Установка промежутка,  в который собирается статистика """
-# - 7 hours
-# Указание отметок
-timeStart = datetime.datetime(2019, 10, 21, 13, 37)
-timeEnd = datetime.datetime(2019, 10, 21, 16, 30)
 
-# Конвертирование в timestamp
-tsStart = int(datetime.datetime.timestamp(timeStart))
-tsEnd = int(datetime.datetime.timestamp(timeEnd))
-print(tsStart)
-print(tsEnd)
+def read_convert_db(year, month, day, start_hour, start_minute, end_hour, end_minute):
+    """ Установка промежутка,  в который собирается статистика """
+    # - 7 hours
+    # Указание отметок
+    timeStart = datetime.datetime(year,
+                                  month,
+                                  day,
+                                  start_hour,
+                                  start_minute)
+    timeEnd = datetime.datetime(year,
+                                month,
+                                day,
+                                end_hour,
+                                end_minute)
 
-""" Запрос списка сердцебиения и подключение к БД """
-conn = sqlite3.connect('data/db21_10_2019_1500_1558.sqlite')
-cursor = conn.cursor()
+    # Конвертирование в timestamp
+    tsStart = int(datetime.datetime.timestamp(timeStart))
+    tsEnd = int(datetime.datetime.timestamp(timeEnd))
 
-cursor.execute("SELECT * FROM heartrate WHERE time BETWEEN ? AND ?", (tsStart + 1, tsEnd))
-hrOut = cursor.fetchall()
+    """ Запрос списка сердцебиения и подключение к БД """
+    conn = sqlite3.connect('data/db21_10_2019_1500_1558.sqlite')
+    cursor = conn.cursor()
 
-""" Запрос списка шагов и закрытие соединения с БД"""
-cursor.execute("SELECT steps, time FROM steps WHERE time BETWEEN ? AND ?", (tsStart, tsEnd))
-stepsOut = cursor.fetchall()
-conn.close()
+    cursor.execute("SELECT * FROM heartrate WHERE time BETWEEN ? AND ?", (tsStart + 1, tsEnd))
+    hrOut = cursor.fetchall()
 
-# Debug
-# print(hrOut)
-# print(hrOut.__len__())
-# print(type(hrOut))
-# print(stepsOut)
-# print(stepsOut.__len__())
-# print(type(stepsOut))
+    """ Запрос списка шагов и закрытие соединения с БД"""
+    cursor.execute("SELECT steps, time FROM steps WHERE time BETWEEN ? AND ?", (tsStart, tsEnd))
+    steps_out = cursor.fetchall()
+    conn.close()
 
-""" Перенос данных из списка в массив, отбросив пустые ячейки """
-"""
-hrOut[i][1] - проход по пульсу
-hrOut[i][0] - проход по ts
+    """ Перенос данных из списка в массив, отбросив пустые ячейки """
+    """
+    hrOut[i][1] - проход по пульсу
+    hrOut[i][0] - проход по ts
+    
+    steps_out[i][0] - проход по шагам
+    steps_out[i][1] - проход по ts
+    """
+    hr_list = [["Date", "Heartrate"]]
+    hr_temp = []
+    for i in range(len(hrOut)):
+        hr_temp.append(str(datetime.datetime.fromtimestamp(hrOut[i][0])))
+        hr_temp.append(hrOut[i][1])
+        hr_list.append(hr_temp[:])
+        hr_temp.clear()
 
-stepsOut[i][0] - проход по шагам
-stepsOut[i][1] - проход по ts
-"""
-hrList = [["Date", "Heartrate"]]
-hrTemp = []
-for i in range(len(hrOut)):
-    hrTemp.append(str(datetime.datetime.fromtimestamp(hrOut[i][0])))
-    hrTemp.append(hrOut[i][1])
-    hrList.append(hrTemp[:])
-    hrTemp.clear()
+    steps_list = [["Date", "Steps"]]
+    steps_temp = []
+    activity_min = []
+    for i in range(len(steps_out)):
+        ts_to_date = str(datetime.datetime.fromtimestamp(steps_out[i][1]))
+        steps_temp.append(ts_to_date)
+        steps_temp.append(steps_out[i][0])
+        steps_list.append(steps_temp[:])
+        steps_temp.clear()
+        if int(steps_out[i][0]) > 0:
+            activity_min.append(ts_to_date[11:])
 
-stepsList = [["Date", "Steps"]]
-stepsTemp = []
-for i in range(len(stepsOut)):
-    stepsTemp.append(str(datetime.datetime.fromtimestamp(stepsOut[i][1])))
-    stepsTemp.append(stepsOut[i][0])
-    stepsList.append(stepsTemp[:])
-    stepsTemp.clear()
+    write_flag_hr = False
+    write_flag_st = False
+    myFile = open('hrdata.csv', 'w')
+    with myFile:
+        writer = csv.writer(myFile)
+        writer.writerows(hr_list)
+        write_flag_hr = True
 
-# Debug
-# print(hrList)
-# print(len(hrList))
-# print(stepsList)
-# print(len(stepsList))
+    myFile = open('stepsdata.csv', 'w')
+    with myFile:
+        writer = csv.writer(myFile)
+        writer.writerows(steps_list)
+        write_flag_st = True
 
-"""How not to do"""
-"""for i in range(len(hrList)):
-    if 10 < i < len(hrList) - 10:
-        hrList[i][1] = int((float(hrList[i - 4][1]) + float(hrList[i - 3][1]) + float(hrList[i - 2][1]) + float(
-            hrList[i - 1][1]) + float(hrList[i][1]) + float(hrList[i + 1][1]) + float(hrList[i + 2][1]) + float(
-            hrList[i + 3][1]) + float(hrList[i + 4][1])) / 9)
-"""
-myFile = open('hrdata.csv', 'w')
-with myFile:
-    writer = csv.writer(myFile)
-    writer.writerows(hrList)
-
-myFile = open('stepsdata.csv', 'w')
-with myFile:
-    writer = csv.writer(myFile)
-    writer.writerows(stepsList)
-
-print("Writing complete")
+    return activity_min
--- ui.py
+++ ui.py
@@ -1,15 +1,58 @@
+import os.path
 from tkinter import *
 
+import activityData
 import plotlytesting
 
 
-def read_db():
-    print("run activityData.py")
+def check_read_db_event():
+    year = int(entryYear.get())
+    month = int(entryMonth.get())
+    day = int(entryDay.get())
+    start_hour = int(entryStartHour.get())
+    start_minute = int(entryStartMinute.get())
+    end_hour = int(entryEndHour.get())
+    end_minute = int(entryEndMinute.get())
+    s_er = '''Введите корректную дату и время.'''
+
+    if year in range(2013, 2099) and month in range(1, 12) and day in range(1, 31):
+        if start_hour in range(0, 23) and end_hour in range(0, 23):
+            if start_minute in range(0, 59) and end_minute in range(0, 59):
+                check = activityData.read_convert_db(year,
+                                                     month,
+                                                     day,
+                                                     start_hour,
+                                                     start_minute,
+                                                     end_hour,
+                                                     end_minute)
+                list_min = "Время физической активности " + str(len(check)) + " мин."
+                show_message(list_min)
+            else:
+                show_message(s_er)
+        else:
+            show_message(s_er)
+    else:
+        show_message(s_er)
 
 
-def show_graph():
-    print("Show graph")
-    plotlytesting.draw_graph()
+def check_graph_file_event():
+    if os.path.exists("hrdata.csv") and os.path.exists("stepsdata.csv"):
+        plotlytesting.draw_graph()
+    else:
+        s_er = '''Не найден hrdata.csv или stepsdata.csv'''
+        show_message(s_er)
+
+
+def show_message(text):
+    error_file = Tk()
+    m_error_file = Message(error_file,
+                           width=200,
+                           text=text,
+                           font="Helvetica 13")
+    m_error_file.pack(expand=True, fill=BOTH)
+    error_file.title("Message")
+    error_file.resizable(False, False)
+    error_file.mainloop()
 
 
 root = Tk()
@@ -108,7 +151,7 @@ buttonRead = Button(root,
 buttonRead.grid(row=10,
                 column=0,
                 sticky=E)
-buttonRead.config(command=read_db)
+buttonRead.config(command=check_read_db_event)
 
 # Кнопка для вывода графика в браузере
 buttonShow = Button(root,
@@ -116,7 +159,7 @@ buttonShow = Button(root,
 buttonShow.grid(row=10,
                 column=1,
                 sticky=E)
-buttonShow.config(command=show_graph)
+buttonShow.config(command=check_graph_file_event)
 Label(root, text='     ').grid(row=11,
                                column=0)
 
